a90d894c64ca577104c5b3bce5f22ba17d1f9e87
grooomary <marykuz0104@gmail.com>
2023-07-28 11:18:04 +0300

    lesson_5_homework

--- advert/views.py
+++ advert/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 # прописываем что можно вернуть на какой-либо запрос
 def index(request):
-    return HttpResponse('Успешно')
+    return render(request, 'index.html')
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
