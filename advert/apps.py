65cdbc9b957698df177c7d4f67be519341e30b10
grooomary <marykuz0104@gmail.com>
2023-08-12 20:32:23 +0300

    lesson_8

--- advert/apps.py
+++ advert/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AdvertConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'advert'
+    verbose_name = 'Объявления'
--- advert/models.py
+++ advert/models.py
@@ -1,12 +1,31 @@
+from django.contrib import admin
 from django.db import models
+from django.utils import timezone
+from django.utils.html import format_html
 
-class Advert(models.Model):
+
+class Advertisement(models.Model):
     title = models.CharField(verbose_name='Название', max_length=128)
     description = models.TextField('Описание')
     price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
     auction = models.BooleanField('Торг', help_text='Отметьте, есть ли торг')
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        if self.create_at.date() == timezone.now().date():
+            created_time = self.create_at.time().strftime("%H:%M:%S")
+            return format_html('<span style="color: green; font-weight: 700;">Сегодня в {}</span>', created_time)
+        return self.create_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+    @admin.display(description='Дата обновления')
+    def updated_date(self):
+        if self.update_at.date() == timezone.now().date():
+            updated_time = self.update_at.time().strftime("%H:%M:%S")
+            return format_html('<span style="color: #C74247; font-weight: 700;">Сегодня в {}</span>', updated_time)
+        return self.update_at.strftime("%d.%m.%Y в %H:%M:%S")
+
     class Meta:
         db_table = 'advertisements'
 
